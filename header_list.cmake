62c967b20ce144d2753b8e1fb29d5a210f56aa2c
Haotian Mo <33168288+junius7even@users.noreply.github.com>
2023-09-17 20:34:53 -0400

    Configure cmake headerlist

---
+++ header_list.cmake
@@ -0,0 +1,12 @@
+set(prefix headers)
+
+set(game_headers
+        ${prefix}apple.h
+        ${prefix}Engine.h
+        ${prefix}Wall.h
+        ${prefix}SnakeSection.h
+        )
+
+set(header_list
+        ${game_headers}
+        )
